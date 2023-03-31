2e0dc5525dcd80dc7edfff41966014dc6c425ebe
Carina Cunha <carina.cunha13@gmail.com>
2023-03-31 19:21:44 -0300

    feat: create a updat route

--- front/src/components/Form.jsx
+++ front/src/components/Form.jsx
@@ -1,18 +1,45 @@
 
 import { useContext } from 'react';
 import MainContext from '../context/MainContext';
-import { postData } from '../services/requests';
+import { postData,  putData } from '../services/requests';
+import moment from 'moment';
 
 function Form(){
-    const { tasks, setTasks } = useContext(MainContext);
+    const { tasks, setTasks, setAllTasks, allTasks, isEditMode, setIsEditMode, idEdit } = useContext(MainContext);
     
     const handleChange = ({ target: { value, name } }) => {
       setTasks((prev) => ({ ...prev, [name]: value }));
     };
 
-    const handleClickAdd = async (e) => {
-      await postData('http://localhost:3000/tasks', tasks)
+    const handleClickAdd = async () => {
+      await postData('http://localhost:3000/tasks', tasks);
+      setAllTasks((prev) => ([ ...prev, tasks ]));
+      setTasks({
+        title: '',
+        description: '',
+        doneDate: '',
+      });
     };
+
+    const handleClickUpdate = async () => {
+      tasks.doneDate = moment(tasks.doneDate).format('YYYY-MM-DD');
+      await putData(`http://localhost:3000/tasks/${idEdit}`, tasks);
+      const updatedTasks = allTasks.reduce((acc, curr) => {
+        if (curr.id === idEdit){
+          curr = tasks;
+        }
+        acc.push(curr);
+          return acc;
+      }, []);
+
+      setAllTasks(updatedTasks);
+      setTasks({
+        title: '',
+        description: '',
+        doneDate: '',
+      });
+      setIsEditMode(false);
+    }
 
     return (
       <section className="task-section">
@@ -26,6 +53,7 @@ function Form(){
               type="text"
               name="title"
               id="title"
+              value={ tasks.title }
               onChange={ handleChange }
               
             />
@@ -35,6 +63,7 @@ function Form(){
               type="text"
               name="description"
               id="description"
+              value={ tasks.description }
               onChange={ handleChange }
               
             />
@@ -45,18 +74,30 @@ function Form(){
               name="doneDate"
               type="text"
               id="date"
+              value={ tasks.doneDate }
               placeholder='AAAA-MM-DD'
               onChange={ handleChange }
-              
             />
         
           <div className="task-form-buttons">
-            <button
-              data-testid="insert-task-btn"
-              onClick={ handleClickAdd }
-            >
-              Salvar
-            </button>
+            {
+              isEditMode ? (
+                <button
+                data-testid="insert-task-btn"
+                onClick={ handleClickUpdate }
+              >
+                Salvar edição
+              </button>
+              ) :(
+                <button
+                data-testid="insert-task-btn"
+                onClick={ handleClickAdd }
+              >
+                Salvar
+              </button>
+              )
+            }
+
           </div>
   
         </form>
