eabbb50fc77fe93e702bc5c241a4562381c88ff2
Carina Cunha <carina.cunha13@gmail.com>
2023-03-31 19:23:41 -0300

    feat: create a delete route

--- front/src/components/Table.jsx
+++ front/src/components/Table.jsx
@@ -1,28 +1,48 @@
 import { useContext } from 'react';
 import MainContext from '../context/MainContext';
 import moment from 'moment';
+import Button from './Button';
+import { deleteData } from '../services/requests';
 
 function Table() {
+  const { allTasks, setAllTasks, setTasks, setIdEdit, setIsEditMode } = useContext(MainContext);
+  
+  const handleUpdateTask = (id) => {
+    const specificTask = allTasks.filter((task) => task.id === id );
+    setTasks({ ...specificTask[0] });
+    setIdEdit(id);
+    setIsEditMode(true);
+  }
 
-  const { allTasks } = useContext(MainContext);
+  const handleDeleteTask = (id) => {
+    deleteData(`http://localhost:3000/tasks/${id}`);
+    const updatedListTasks = allTasks.filter((task) => {
+      return task.id !== id;
+    });
+
+    setAllTasks(updatedListTasks);
+  }
 
   return (
     <table>
-      <thread>
-        <th className="field-title">Título</th>
-        <th className="field-decription">Descrição</th>
-        <th className="field-date">Data de conclusão</th>
+      <thead>
+        <tr>
+          <th className="field-title">Título</th>
+          <th className="field-decription">Descrição</th>
+          <th className="field-date">Data de conclusão</th>
+        </tr>
+      </thead>
       <tbody>
         { allTasks.map((task) => (
-          <tr>
+          <tr key={ task.id }>
             <td>{ task.title }</td>
             <td>{ task.description }</td>
             <td>{ moment(task.doneDate).format('l') }</td>
+            <td><Button name='Editar' callback={()=> handleUpdateTask(task.id) }/></td>
+            <td><Button name='Deletar' callback={()=> handleDeleteTask(task.id) }/></td>
           </tr>
         ))}
-      </tbody>
-            
-      </thread>
+      </tbody>     
     </table>
   )
 }
