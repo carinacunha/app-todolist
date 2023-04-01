36e736969e334261381e9bb53b6d9f5a0c826853
Carina <carina.cunha13@gmail.com>
2023-04-01 07:56:59 -0300

    feat: create a controller to user

---
+++ SistemadeTarefa/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemadeTarefa.Models;
+using SistemadeTarefas.Models;
+
+namespace SistemadeTarefa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios() 
+        {
+            return Ok();
+        }
+    }
+}
