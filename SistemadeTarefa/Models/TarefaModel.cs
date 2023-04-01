b5cc37995a1b5ec1c65ad61b86e8fbeb22423aad
Carina <carina.cunha13@gmail.com>
2023-04-01 07:56:41 -0300

    feat: create a model to task

---
+++ SistemadeTarefa/Models/TarefaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemadeTarefas.Models
+{
+    public class TarefaModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Descricao { get; set; }
+
+        public int status { get; set; }
+    }
+}
--- SistemadeTarefa/Models/UsuarioModels.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SistemadeTarefa.Models
-{
-    public class UsuarioModels
-    {
-        public string Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
-    }
-}
