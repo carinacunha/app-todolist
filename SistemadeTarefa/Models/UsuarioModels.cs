230d26a12e463140a0f2e72a4edb84a406b34ca8
Carina <carina.cunha13@gmail.com>
2023-04-01 07:38:37 -0300

    feat: create a model file

---
+++ SistemadeTarefa/Models/UsuarioModels.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemadeTarefa.Models
+{
+    public class UsuarioModels
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
