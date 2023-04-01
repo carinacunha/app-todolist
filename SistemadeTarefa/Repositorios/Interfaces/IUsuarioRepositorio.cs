f8fbdab40a3fd89c4a97003604644b743c2c4dd6
Carina <carina.cunha13@gmail.com>
2023-04-01 08:21:02 -0300

    feat: create a interface to user

---
+++ SistemadeTarefa/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using SistemadeTarefa.Models;
+
+namespace SistemadeTarefa.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<UsuarioModel> BucarPorId(int id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<bool> Apagar(int id);
+    }
+}
