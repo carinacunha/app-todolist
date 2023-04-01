009eb87ef9f5e5e673a00b6a9e49f242880205e6
Carina <carina.cunha13@gmail.com>
2023-04-01 08:13:02 -0300

    feat: create a db context

---
+++ SistemadeTarefa/Data/SistemaTarefasDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemadeTarefa.Models;
+using SistemadeTarefas.Models;
+
+namespace SistemadeTarefa.Data
+{
+    public class SistemaTarefasDBContext : DbContext
+    {
+        public SistemaTarefasDBContext(DbContextOptions<SistemaTarefasDBContext> options)
+            :base(options)
+        {
+        }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
