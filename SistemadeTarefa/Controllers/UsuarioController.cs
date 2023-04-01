fba8f54bf9486b86bdf4ec0191c5c4aa5d86b3cf
Carina <carina.cunha13@gmail.com>
2023-04-01 17:24:43 -0300

    feat: create a get, get by id and post usuario route

--- SistemadeTarefa/Controllers/UsuarioController.cs
+++ SistemadeTarefa/Controllers/UsuarioController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SistemadeTarefa.Models;
-using SistemadeTarefas.Models;
+using SistemadeTarefa.Repositorios.Interfaces;
 
 namespace SistemadeTarefa.Controllers
 {
@@ -9,10 +9,29 @@ namespace SistemadeTarefa.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio) {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
         [HttpGet]
-        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios() 
+        public async Task <ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios() 
         {
+            List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
             return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BucarPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+            return Ok(usuario);
         }
     }
 }
