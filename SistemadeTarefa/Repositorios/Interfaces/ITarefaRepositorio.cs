1d07836cc70dae56eb6222d28a8beec8803e92da
Carina <carina.cunha13@gmail.com>
2023-04-01 17:56:43 -0300

    feat: create a task repository

---
+++ SistemadeTarefa/Repositorios/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿
+using SistemadeTarefas.Models;
+
+namespace SistemadeTarefa.Repositorios.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        Task<List<TarefaModel>> BuscarTodasTarefas();
+        Task<TarefaModel> BucarPorId(int id);
+        Task<TarefaModel> Adicionar(TarefaModel tarefa);
+        Task<TarefaModel> Atualizar(TarefaModel tarefa, int id);
+        Task<bool> Apagar(int id);
+    }
+}
