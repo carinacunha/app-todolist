f83619f5aa5e929da7da0ddd9e56cc0c9c03d869
Carina Cunha <carina.cunha13@gmail.com>
2023-04-02 17:29:52 -0300

    fix: update port

---
+++ front-end/src/components/Form.jsx
@@ -0,0 +1,107 @@
+import { useContext } from 'react';
+import MainContext from '../context/MainContext';
+import { postData,  putData } from '../services/requests';
+import moment from 'moment';
+
+function Form(){
+    const { tasks, setTasks, setAllTasks, allTasks, isEditMode, setIsEditMode, idEdit } = useContext(MainContext);
+    
+    const handleChange = ({ target: { value, name } }) => {
+      setTasks((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleClickAdd = async () => {
+      await postData('http://localhost:5209/api/Task', tasks);
+      setAllTasks((prev) => ([ ...prev, tasks ]));
+      setTasks({
+        title: '',
+        description: '',
+        doneDate: '',
+      });
+    };
+
+    const handleClickUpdate = async () => {
+      tasks.doneDate = moment(tasks.doneDate).format('YYYY-MM-DD');
+      await putData(`http://localhost:5209/api/Task/${idEdit}`, tasks);
+      const updatedTasks = allTasks.reduce((acc, curr) => {
+        if (curr.id === idEdit){
+          curr = tasks;
+        }
+        acc.push(curr);
+          return acc;
+      }, []);
+
+      setAllTasks(updatedTasks);
+      setTasks({
+        title: '',
+        description: '',
+        doneDate: '',
+      });
+      setIsEditMode(false);
+    }
+
+    return (
+      <section className="task-section">
+        <form className="task-form"
+          onSubmit={ (event)=>  event.preventDefault() }
+        >
+        <p>Cadastre sua tarefa</p>
+          <label>Título:</label>
+            <input
+              className="title"
+              type="text"
+              name="title"
+              id="title"
+              value={ tasks.title }
+              onChange={ handleChange }
+              
+            />
+          <label>Descrição:</label>
+            <input
+              className="description"
+              type="text"
+              name="description"
+              id="description"
+              value={ tasks.description }
+              onChange={ handleChange }
+              
+            />
+
+          <label>Data de conclusão:</label>   
+            <input
+              className="done-date"
+              name="doneDate"
+              type="text"
+              id="date"
+              value={ tasks.doneDate }
+              placeholder='AAAA-MM-DD'
+              onChange={ handleChange }
+            />
+        
+          <div className="task-form-buttons">
+            {
+              isEditMode ? (
+                <button
+                data-testid="insert-task-btn"
+                onClick={ handleClickUpdate }
+              >
+                Salvar edição
+              </button>
+              ) :(
+                <button
+                data-testid="insert-task-btn"
+                onClick={ handleClickAdd }
+              >
+                Salvar
+              </button>
+              )
+            }
+
+          </div>
+  
+        </form>
+    </section>
+    )
+}
+
+export default Form;
---
+++ front-end/src/components/Table.jsx
@@ -0,0 +1,50 @@
+import { useContext } from 'react';
+import MainContext from '../context/MainContext';
+import moment from 'moment';
+import Button from './Button';
+import { deleteData } from '../services/requests';
+
+function Table() {
+  const { allTasks, setAllTasks, setTasks, setIdEdit, setIsEditMode } = useContext(MainContext);
+  
+  const handleUpdateTask = (id) => {
+    const specificTask = allTasks.filter((task) => task.id === id );
+    setTasks({ ...specificTask[0] });
+    setIdEdit(id);
+    setIsEditMode(true);
+  }
+
+  const handleDeleteTask = (id) => {
+    deleteData(`http://localhost:5209/api/Task/${id}`);
+    const updatedListTasks = allTasks.filter((task) => {
+      return task.id !== id;
+    });
+
+    setAllTasks(updatedListTasks);
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th className="field-title">Título</th>
+          <th className="field-decription">Descrição</th>
+          <th className="field-date">Data de conclusão</th>
+        </tr>
+      </thead>
+      <tbody>
+        { allTasks.map((task) => (
+          <tr key={ task.id }>
+            <td>{ task.title }</td>
+            <td>{ task.description }</td>
+            <td>{ moment(task.doneDate).format('l') }</td>
+            <td><Button name='Editar' callback={()=> handleUpdateTask(task.id) }/></td>
+            <td><Button name='Deletar' callback={()=> handleDeleteTask(task.id) }/></td>
+          </tr>
+        ))}
+      </tbody>     
+    </table>
+  )
+}
+
+export default Table
